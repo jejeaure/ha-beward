9b4b4785218356abad2224d08c6082ee85921bd3
Andrey Khrolenok <andrey@khrolenok.ru>
2020-05-29 12:44:37 +0300

    Fix setup.py errors

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ import sys
 from setuptools import find_packages, setup
 from setuptools.command.test import test as TestCommand
 
-from custom_components import beward
-
 
 class PyTest(TestCommand):
     """PyTest controller."""
@@ -21,7 +19,7 @@ class PyTest(TestCommand):
         """Finalize test command options."""
         TestCommand.finalize_options(self)
         # we don't run integration tests which need an actual Beward device
-        self.test_args = ['-m', 'not integration']
+        self.test_args = ["-m", "not integration"]
         self.test_suite = True
 
     # pylint: disable=import-outside-toplevel,import-error
@@ -38,7 +36,7 @@ class PyTest(TestCommand):
 def load_requirements(filename: str) -> list:
     """Load requirements from file."""
     path = os.path.join(os.path.dirname(__file__), filename)
-    imp = re.compile(r'^(-r|--requirement)\s+(\S+)')
+    imp = re.compile(r"^(-r|--requirement)\s+(\S+)")
     reqs = []
     with open(path, encoding="utf-8") as fptr:
         for i in fptr:
@@ -52,21 +50,28 @@ def load_requirements(filename: str) -> list:
     return reqs
 
 
-PROJECT_PACKAGE_NAME = 'ha-beward'
+PROJECT_PACKAGE_NAME = "ha-beward"
 
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
-REQUIREMENTS = load_requirements('requirements.txt')
-TEST_REQUIREMENTS = load_requirements('requirements-tests.txt')
+MODULES = list(map(__import__, PACKAGES))
+VERSION = ""
+for i in MODULES:
+    if hasattr(i, "VERSION"):
+        VERSION = i.VERSION
+        break
+
+REQUIREMENTS = load_requirements("requirements.txt")
+TEST_REQUIREMENTS = load_requirements("requirements-tests.txt")
 
 setup(
     name=PROJECT_PACKAGE_NAME,
-    version=beward.VERSION,
+    version=VERSION,
     packages=PACKAGES,
     include_package_data=True,
     zip_safe=False,
     install_requires=REQUIREMENTS,
-    cmdclass={'pytest': PyTest},
+    cmdclass={"pytest": PyTest},
     test_suite="tests",
     tests_require=TEST_REQUIREMENTS,
 )
